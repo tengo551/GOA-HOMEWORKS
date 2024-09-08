c934d5bbec4a6fcda5c27c5e7a304a3ef69c0b11
tengo551 <luka49381@gmail.com>
2024-09-08 19:08:34 +0400

    commit

---
+++ day 24/homework/homework.py
@@ -0,0 +1,9 @@
+#დავალება: მომხმარებელს შემოატანინეთ 2 სახელი და შემდეგ შეადარეთ, თე სახელები უდრიან ერთმანეთს მაშინ დაპრინტეთ True, ხოლო თუ არ უდრიან დაპრინტეთ False. print ფუნქცით გაკ 
+name1=input("enter your name:")
+name2=input("enter your name:")
+
+
+while   name1 != name2:
+    print(True)
+else: 
+    print(False)
