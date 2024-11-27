a86b8e4ff0d29969adf733adc9561815b55a1212
tengo551 <luka49381@gmail.com>
2024-11-27 19:49:44 +0400

    commit

---
+++ day 31/classwork/index.py
@@ -0,0 +1,15 @@
+def sumerfuction(n):
+    c=0
+    for i in range(n):
+        c+=i 
+    print(c)
+sumerfuction(3)
+sumerfuction(5)
+sumerfuction(50)
+sumerfuction(9)
+sumerfuction(6)
+sumerfuction(7)
+sumerfuction(1)
+sumerfuction(2)
+sumerfuction(8)
+sumerfuction(45)
---
+++ day 31/homework/index.py
@@ -0,0 +1,19 @@
+def calculator(a,b,operator):
+    if operator=="/":
+        print(a/b)
+    if operator=="*":
+        print(a*b)
+    if operator=="-":
+        print(a-b)
+    if operator=="+":
+        print(a+b)
+calculator(1,2,"+")
+def fartobi(a,b,):
+    print(a*b)
+print(max([12,3354,458456,6584678789,5456897589060,1]))    
+print(min([12,3354,458456,6584678789,5456897589060,1])) 
+def cifri(a):
+    if a%2==0:
+        print("luwia")
+    else:
+        print("kenti")          
